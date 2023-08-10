b331bd6759f2155616b46a8cfb6cba6c9007ebfc
Mert Şişmanoğlu <mert190737fb@gmail.com>
2023-08-10 16:51:24 +0300

    fix: Turn generatePlate to async function

--- src/controllers/api.controller.js
+++ src/controllers/api.controller.js
@@ -1,4 +1,10 @@
-function generatePlate(params) {}
+const Canvas = require("../util/Canvas")
+
+async function generatePlate(req,res) {
+    const plate = new Canvas(1750, 415)
+    await plate.fill("Deneme", "Deutschland_1")
+    plate.export("abc")
+}
 
 module.exports = {
     generatePlate,
