02c3343c745f1d490a6cd315a86106a7bfcc5650
Mert Şişmanoğlu <mert190737fb@gmail.com>
2023-08-10 17:05:51 +0300

    fix: Change post to get on api route

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,6 +1,6 @@
 const router = require("express").Router()
 const { generatePlate } = require("../../controllers/api.controller")
 
-router.post("/generatePlate", generatePlate)
+router.get("/generatePlate", generatePlate)
 
 module.exports = router
