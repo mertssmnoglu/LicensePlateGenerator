e7cd21ceaa32dc64c84cf17a3a33b4017dc285e6
Mert Şişmanoğlu <mert190737fb@gmail.com>
2023-08-10 17:06:12 +0300

    feat: Add api route to app.js

--- src/app.js
+++ src/app.js
@@ -1,32 +1,19 @@
 const express = require("express")
 const app = express()
 const port = 3000
-// const generatePlate = require("./generatePlate.js")
-const mainRouter = require("./routes/index.js")
 const { logger } = require("./middlewares")
 
 // Middlewares
 app.use(express.static("public"))
 app.use(logger)
 
+// Routers
+const mainRouter = require("./routes/index.js")
+const apiRouter = require("./routes/api/index.js")
 app.use("/", mainRouter)
+app.use("/api", apiRouter)
 
-// app.get("/generatePlate", async (req, res) => {
-//     let plate = req.query.plate
-//     let plateText = req.query.plateText
-//     let plateType = req.query.plateType
-//     let fontSize = req.query.fontSize
-
-//     if (plate === "" || plate === undefined) {
-//         return res.json({ error: "Plaka Uyruğu Girilmedi" })
-//     } else if (plateText === "" || plateText === undefined) {
-//         return res.json({ error: "Plaka Yazısı Girilmedi" })
-//     }
-
-//     await generatePlate(plate, plateText, plateType, fontSize)
-//     res.sendFile(__dirname + "/public/image.png")
-// })
-
+// 404
 app.get("*", function (req, res) {
     res.status(404).json({
         err: "http_404",
