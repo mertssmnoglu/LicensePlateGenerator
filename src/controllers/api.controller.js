fe33f20910c1f57173697df398235258d69b0f0d
Mert Şişmanoğlu <mert190737fb@gmail.com>
2023-08-10 17:27:05 +0300

    feat: Api controller canvas creation

--- src/controllers/api.controller.js
+++ src/controllers/api.controller.js
@@ -1,9 +1,11 @@
 const Canvas = require("../util/Canvas")
 
-async function generatePlate(req,res) {
+async function generatePlate(req, res) {
+    const plateName = `plate_${Date.now()}`
     const plate = new Canvas(1750, 415)
-    await plate.fill("Deneme", "Deutschland_1")
-    plate.export("abc")
+    await plate.fill("Deneme", 308, "Deutschland_1")
+    plate.export(plateName)
+    res.sendFile(`${plateName}.png`, { root: "public" })
 }
 
 module.exports = {
