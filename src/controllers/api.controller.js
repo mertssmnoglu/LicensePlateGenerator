5e4dafdacd69fd0655946a9fc78f9adb24c70c39
Mert Şişmanoğlu <mert190737fb@gmail.com>
2023-08-18 13:22:03 +0300

    feat: Add queryStrings to dynamic plate generation

--- src/controllers/api.controller.js
+++ src/controllers/api.controller.js
@@ -1,11 +1,19 @@
 const Canvas = require("../util/Canvas")
 
 async function generatePlate(req, res) {
-    const plateName = `plate_${Date.now()}`
+    const { text } = req.query
+
+    if (!text || text.length < 1)
+        return res.status(401).json({
+            status: 401,
+            message: "Plate text must at least 1 character",
+        })
+
+    const plateName = `plate_${text}-${Date.now()}`
     const plate = new Canvas(1750, 415)
-    await plate.fill("Deneme", 308, "Deutschland_1")
+    await plate.fill(text, 308, "Deutschland_1")
     plate.export(plateName)
-    res.sendFile(`${plateName}.png`, { root: "build" })
+    res.sendFile(`${plateName}.png`, { root: "build/plates" })
 }
 
 module.exports = {
